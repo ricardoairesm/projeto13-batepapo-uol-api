bd62deef0343fe12455e19a7c033ed5591d54e16
ricardoairesm <ricardoaamb@usp.br>
2023-01-13 19:14:30 -0300

    GET and POST for route /messages

--- src/app.js
+++ src/app.js
@@ -11,40 +11,20 @@ app.use(express.json());
 app.use(cors());
 const PORT = 5000;
 
-const hora = dayjs().format('HH:mm:ss')
 
-const messageSchema = Joi.object().keys({
-    type: Joi.string().valid('message', 'private-message').required(),
+const validData = (schema, data) => !schema.validate(data).error;
+
+const messageSchema = Joi.object({
+    type: Joi.string().valid('message', 'private_message').required(),
     to: Joi.string().required(),
     text: Joi.string().required(),
-    from: Joi.string().required(),
 });
 
-const nameSchema = Joi.object().keys({
-    name:Joi.string().required()
+const nameSchema = Joi.object({
+    name: Joi.string().required().invalid('Todos'),
 })
 
-
-const validateMessage = (message) => {
-    const result = Joi.validate(message, messageSchema);
-    if (result.error) {
-        throw result.error;
-    }
-    return result.value;
-}
-
-const validateName = (name) => {
-    const result = Joi.validate(name, nameSchema);
-    if (result.error) throw result.error;
-    return result.value;
-}
-try {
-    const validMessage = validateMessage({ type: 'message' });
-    console.log(validMessage); // { type: 'message' }
-} catch (error) {
-    console.log("Mensagem com formato inválido!");
-}
-
+const headerSchema = Joi.string().required();
 
 
 // conectando ao banco
@@ -55,56 +35,77 @@ await mongoClient.connect();
 db = mongoClient.db();
 
 
-app.get("/participants", async(req, res) => {
+app.get("/participants", async (req, res) => {
     // buscando usuários
     try {
         const userList = await db.collection("participants").find({}).toArray();
-        return res.send(data);
+        return res.send(userList);
     } catch (error) {
         console.error("erro na rota get /participants", error);
         return res.sendStatus(500);
     }
 });
 
-app.post("/participants", async(req, res) => {
+app.post("/participants", async (req, res) => {
     // inserindo usuário
     const { name } = req.body;
     //validando usuario
-    try {
-        const validName = validateName(req.body);
-        console.log(validName); 
-    } catch (error) {
-        console.log("Nome com formato inválido!");
-        res.sendStatus(422);
-        return;
+    if (!validData(nameSchema, req.body)) {
+        console.log("Insira um nome válido")
+        return res.sendStatus(422);
     }
     //checando se o nome de usuario já existe
-    const user = await db.collection("participants").findOne(name);
-    if (user){
-        res.sendStatus(409);
-        return;
-    }; 
-    //construindo os objetos a serem mandados pro nosso banco de dados
-    const entryMessageFormat = {from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: hora};
-    const userFormat = {name: name, lastStatus: Date.now().valueOf()};
-    try{
-        await db.collection('participants').insertOne(userFormat);
-        await db.collection('messages').insertOne(entryMessageFormat);
-    
-    }catch(error){
+    const user = await db.collection("participants").findOne(req.body);
+    if (user) {
+        return res.sendStatus(409);
+    };
+
+    const now = dayjs();
+    //mandando as informações pro banco de dado
+    try {
+        await db.collection('participants').insertOne({ ...req.body, lastStatus: now.valueOf() });
+        await db.collection('messages').insertOne({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: now.format('HH:mm:ss') });
+        return res.sendStatus(201);
+    } catch (error) {
         console.error("erro na rota post /participants", error);
         return res.sendStatus(500);
     }
-  
+
 });
 
-app.get("/messages", (req, res) => {
+app.get("/messages", async (req, res) => {
     // buscando mensagens
+    const { user } = req.headers.user;
+    const limit = req.query.limit;
+    try {
+        const messageList = await db.collection("messages").find({ $or: [{ from: req.headers.user }, { to: req.headers.user }, { to: 'Todos' }] }).toArray();
+        if (limit) {
+            const sizeLimited = messageList.splice(0, limit);
+            return res.send(sizeLimited);
+        }
+        return res.send(messageList);
+    } catch (error) {
+        console.error("erro na rota get /messages", error);
+        return res.sendStatus(500);
+    }
 
 });
 
-app.post("/messages", (req, res) => {
+app.post("/messages", async (req, res) => {
     // inserindo mensagens
+    const user = req.headers.user;
+    const hora = dayjs().format('HH:mm:ss');
+    const completeMessage = { ...req.body, from: user, time: hora };
+    if (!validData(messageSchema, req.body) || !validData(headerSchema, user)) {
+        return res.sendStatus(422);
+    }
+    try {
+        await db.collection('messages').insertOne(completeMessage);
+        return res.sendStatus(201);
+    } catch (error) {
+        console.error("erro na rota post /participants", error);
+        return res.sendStatus(500);
+    }
 });
 
 app.post("/status", (req, res) => {
