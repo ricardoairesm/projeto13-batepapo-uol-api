f16d4475d7da488751e84b0435c5bbe5979f0127
ricardoairesm <ricardoaamb@usp.br>
2023-01-14 15:41:28 -0300

    bug fix 2.0 (splicing in the wrong order)

--- src/app.js
+++ src/app.js
@@ -77,15 +77,14 @@ app.get("/messages", async (req, res) => {
     // buscando mensagens
     const { user } = req.headers.user;
     const limit = req.query.limit;
-    if(limit<=0 || !limit){
+    if(limit<=0){
         return res.sendStatus(422);
     }
     try {
         const messageList = await db.collection("messages").find({ $or: [{ from: req.headers.user }, { to: req.headers.user }, { to: 'Todos' }] }).toArray();
         if (limit>0) {
-            const sizeLimited = messageList.splice(0, limit);
-            const reversed = sizeLimited.reverse();
-            return res.send(reversed);
+            const sizeLimited = messageList.reverse().splice(0, limit);
+            return res.send(sizeLimited);
         }
         return res.send(messageList.reverse());
     } catch (error) {
